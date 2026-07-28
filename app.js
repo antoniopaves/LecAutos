06ab60e7c3d32f351cfb003d308acace2850ff7a
antoniopaves <antoniopaves877@gmail.com>
2026-07-28 15:57:53 -0400

    cambio en las rutas de auth

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'src')));
 
 app.get('/', (req, res) => {
-    res.render('login');
+    res.redirect('autos');
 });
 
 app.use('/auth', authRouter);
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -3,6 +3,10 @@ import { login } from "../controller/authController.js";
 
 const router = Router();
 
+router.get("/", (req, res) => {
+    res.render("login");
+});
+
 router.post("/login", login);
 
 export default router;
